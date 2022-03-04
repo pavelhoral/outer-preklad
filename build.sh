375ae0570f37a375c6e79e8ca5c7aa3cab044641
Pavel Horal <pavel.horal@orchitech.cz>
2022-03-04 20:46:15 +0100

    Build script WIP.

--- build.sh
+++ build.sh
@@ -20,6 +20,14 @@ RESPONSE_FILE="target/repsonse.txt"
 cat > $RESPONSE_FILE << EOM
 "$BUILD_ROOT/target/Text_en.uasset" "../../../Indiana/Content/Exported/BaseGame/Localized/EN/Text/Text_en.uasset"
 "$BUILD_ROOT/target/Text_en.uexp" "../../../Indiana/Content/Exported/BaseGame/Localized/EN/Text/Text_en.uexp"
+"$BUILD_ROOT/target/OBSTOW_Family.uasset" "../../../Indiana/Content/UI/Library/Font/OBSTOW_Family.uasset"
+"$BUILD_ROOT/target/OBSTOW_Family.uexp" "../../../Indiana/Content/UI/Library/Font/OBSTOW_Family.uext"
+"$BUILD_ROOT/target/OBSTOW_Bold.uasset" "../../../Indiana/Content/UI/Library/Font/OBSTOW_Bold.uasset"
+"$BUILD_ROOT/target/OBSTOW_Bold.uexp" "../../../Indiana/Content/UI/Library/Font/OBSTOW_Bold.uexp"
+"$BUILD_ROOT/target/OBSTOW_Bold.ufont" "../../../Indiana/Content/UI/Library/Font/OBSTOW_Bold.ufont"
+"$BUILD_ROOT/target/TCM_Regular.uasset" "../../../Indiana/Content/UI/Library/Font/TCM_Regular.uasset"
+"$BUILD_ROOT/target/TCM_Regular.uexp" "../../../Indiana/Content/UI/Library/Font/TCM_Regular.uexp"
+"$BUILD_ROOT/target/TCM_Regular.ufont" "../../../Indiana/Content/UI/Library/Font/TCM_Regular.ufont"
 EOM
 
 $UNREALPAK_BIN "$BUILD_ROOT/target/zzCS-WindowsNoEditor.pak" -Create="$BUILD_ROOT/$RESPONSE_FILE"
